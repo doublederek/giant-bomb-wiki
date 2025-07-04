5035d2bb5d971658c1eb78285650bf4073f66196
Derek Ledoux <kingsquestband@gmail.com>
2025-07-04 18:48:32 -0400

    adding protection for template injection, created helper function that can be used in any template file

--- skins/GiantBomb/includes/partials/game-card.php
+++ skins/GiantBomb/includes/partials/game-card.php
@@ -1,18 +1,20 @@
 <?php
+require_once __DIR__ . '/../helpers.php';
+
 if (!isset($gameData) || !is_array($gameData)) return;
 $index = isset($index) ? intval($index) : 0;
 ?>
 <div 
     class="game-card-base"
-    id="game-card-<?php echo $index + 1; ?>"
-    style="background-image: url('<?php echo $gameData['img']; ?>');"
+    id="game-card-<?php echo h($index + 1); ?>"
+    style="background-image: url('<?php echo h($gameData['img']); ?>');"
 >
     
     <div class="info-continer">
         <div class="game-title-and-date">
-            <h2 class="highlight"><?php echo $gameData['title']; ?></h2>
-            <h4><?php echo $gameData['date']; ?></h4>
+            <h2 class="highlight"><?php echo h($gameData['title']); ?></h2>
+            <h4><?php echo h($gameData['date']); ?></h4>
         </div>
-        <p><?php echo $gameData['desc']; ?></p>
+        <p><?php echo h($gameData['desc']); ?></p>
     </div>
 </div>
