2862b0877ba0b2cb7b3c3dc9efc2d4f38e0219d4
Rene Ruiz <reneruiz@gmail.com>
2025-07-04 03:58:56 -0400

    fix: nested html inside body element (#17)

--- skins/GiantBomb/includes/GiantBombTemplate.php
+++ skins/GiantBomb/includes/GiantBombTemplate.php
@@ -2,20 +2,11 @@
 class GiantBombTemplate extends BaseTemplate {
     public function execute() {
         ?>
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <?php echo $this->get('headelement'); ?>
-            </head>
-            <body>
-                <h1>Giant Bomb Wiki</h1>
-                <div
-                    data-vue-component="VueExampleComponent"
-                    data-label="An example vue component with props">
-                </div>
-                <?php echo $this->get('trailelement'); ?>
-            </body>
-        </html>
+        <h1>Giant Bomb Wiki</h1>
+        <div
+            data-vue-component="VueExampleComponent"
+            data-label="An example vue component with props">
+        </div>
         <?php
     }
 }
