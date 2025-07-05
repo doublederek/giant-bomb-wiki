3f8d29d24340cda2ceb271a095949f3efec7278f
Derek Ledoux <kingsquestband@gmail.com>
2025-07-05 08:30:45 -0400

    Adding Mustache templating for the Game Card

--- skins/GiantBomb/includes/partials/game-card.php
+++ skins/GiantBomb/includes/partials/game-card.php
@@ -1,20 +1,23 @@
 <?php
-require_once __DIR__ . '/../helpers.php';
+use MediaWiki\Html\TemplateParser;
 
-if (!isset($gameData) || !is_array($gameData)) return;
-$index = isset($index) ? intval($index) : 0;
-?>
-<div 
-    class="game-card-base"
-    id="game-card-<?php echo h($index + 1); ?>"
-    style="background-image: url('<?php echo h($gameData['img']); ?>');"
->
-    
-    <div class="info-continer">
-        <div class="game-title-and-date">
-            <h2 class="highlight"><?php echo h($gameData['title']); ?></h2>
-            <h4><?php echo h($gameData['date']); ?></h4>
-        </div>
-        <p><?php echo h($gameData['desc']); ?></p>
-    </div>
-</div>
+if ( !isset( $gameData ) || !is_array( $gameData ) ) {
+    return;
+}
+
+$index = isset( $index ) ? intval( $index ) + 1 : 1;
+
+// Prepare data for Mustache
+$tplData = [
+    'index' => $index,
+    'img'   => $gameData['img'],
+    'title' => $gameData['title'],
+    'date'  => $gameData['date'],
+    'desc'  => $gameData['desc'],
+];
+
+// Create TemplateParser instance
+$templateParser = new TemplateParser( __DIR__ );
+
+// Render and echo
+echo $templateParser->processTemplate( 'game-card', $tplData );
